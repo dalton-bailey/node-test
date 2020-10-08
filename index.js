3ab7ff304223f5d209de3b260fc9c912d0730693
Bailey Dalton <baidalton@gmail.com>
2020-10-07 19:44:55 -0600

    changes

--- index.js
+++ index.js
@@ -1,14 +1,15 @@
 var express = require("express");
 var app = express();
 
+const port = process.env.PORT || 3000
 
 app.createServer(function (req, res) {
     res.writeHead(200, {"Content-Type": "text/plain"})
     res.end("Hello World!")
-}).listen(process.env.PORT)
+}).listen(port)
 
-app.set('view engine', 'pug')
+// app.set('view engine', 'pug')
 
-app.get('views', function (req, res) {
-    res.render('index', { title: 'Hey', message: 'Hello there!' })
-  })
+// app.get('views', function (req, res) {
+//     res.render('index', { title: 'Hey', message: 'Hello there!' })
+//   })
