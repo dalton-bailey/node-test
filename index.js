5e2c7d15b9926d2bb96b09a6401d907862c234e2
Bailey Dalton <baidalton@gmail.com>
2020-10-07 20:11:01 -0600

    path change

--- index.js
+++ index.js
@@ -1,17 +1,14 @@
 var express = require("express");
-const path = require('path')
 var app = express();
 
 const port = process.env.PORT || 3000
 
-app.set('views', path.join(__dirname, 'views'))
+app.set('views', './views')
 app.set('view engine', 'pug')
 
 app.get('/', function (req, res) {
     res.render('index', { title: 'Hey', message: 'Hello there!' })
   })
-
-
 
 app.listen(port, () => {
  console.log("Server running on port 3000");
