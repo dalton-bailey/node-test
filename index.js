adf842140d2db9c70cd0fec64118566db0482b73
Bailey Dalton <baidalton@gmail.com>
2020-10-08 21:10:22 -0600

    homepage

--- index.js
+++ index.js
@@ -1,14 +1,19 @@
 var express = require("express");
 var app = express();
+var path = require('path')
 
 const port = process.env.PORT || 3000
 
 app.set('views', './views')
 app.set('view engine', 'pug')
 
+app.use("/public", express.static(path.join(__dirname, 'public')));
+
 app.get('/', function (req, res) {
-    res.render('index', { title: 'Hey', message: 'Hello there!' })
+    res.render('index', { title: 'Hey', message: 'It is working' })
   })
+
+
 
 app.listen(port, () => {
  console.log("Server running on port 3000");
